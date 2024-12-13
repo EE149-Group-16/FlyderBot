4992e59942d8500c2ecf5bca9e58d0be2921b59b
1zj19 <isabel@wufamily.net>
2024-12-12 18:40:30 -0800

    jacob edits: struct for propeller. running

--- robot-lib/src/propeller.c
+++ robot-lib/src/propeller.c
@@ -9,33 +9,45 @@
 #include <pico/stdlib.h>
 #include <hardware/pwm.h>
 
-void propeller_init() {
-    gpio_init(PROPELLER_PIN);
-    gpio_set_dir(PROPELLER_PIN, GPIO_OUT);
-    // set the pin to output pwm signals
-    gpio_set_function(PROPELLER_PIN, GPIO_FUNC_PWM);
-    // configure PWM slice and get slice num that controls speed
-    uint32_t slice = pwm_gpio_to_slice_num(PROPELLER_PIN);
-    pwm_config cfg = pwm_get_default_config();
-    pwm_config_set_clkdiv(&cfg, CLOCK_DIVIDER);
-    pwm_config_set_wrap(&cfg, WRAP_VAL);
-    pwm_init(slice, &cfg, true);
+propeller_t* propeller_init(uint8_t pin) {
+    propeller_t* propeller = malloc(sizeof(propeller_t));
 
+    gpio_init(pin);
+    gpio_set_dir(pin, GPIO_OUT);
+    // set the pin to output pwm signals
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+    // configure PWM slice and get slice num that controls speed
+    propeller->pin = pin;
+    propeller->slice = pwm_gpio_to_slice_num(pin);
+    propeller->channel = pwm_gpio_to_channel(pin);
+    pwm_config cfg = pwm_get_default_config();
+    // pwm_config_set_clkdiv(&cfg, CLOCK_DIVIDER);
+    pwm_config_set_wrap(&cfg, WRAP_VAL);
+    pwm_init(propeller->slice, &cfg, true);
+    
+    return propeller;
 }
 
-void propeller_set_power(uint16_t power_level) {
-    // power_level is btwn 0 and 1.0
-    // specs say pwm btwn 0 and max throttle is 1ms to 2ms. set in propeller.h
+void propeller_arm_seq(propeller_t* prop) {
+    // ARMING SEQUENCE
+    pwm_set_chan_level(prop->slice, prop->channel, 1000*WRAP_VAL/20000); // MIN THROTTLE
+    sleep_ms(10000);
+}
+
+void propeller_set_power(propeller_t* prop, uint16_t power_level) {
+
+        // power_level is btwn 0 and 1.0
+    // specs say pwm btwn 0 and max th5000rottle is 1ms to 2ms. set in propeller.h
     // uint16_t level = power_level* ((MAX_PW - MIN_PW)/100) + MIN_PW;
     // pwm_set_gpio_level(PROPELLER_PIN, level);
-
-    // ARMING SEQUENCE
-    pwm_set_gpio_level(PROPELLER_PIN, 1800);
-    sleep_ms(500);
-    pwm_set_gpio_level(PROPELLER_PIN, 1500); // THROTTLE AT ZERO
-    sleep_ms(500);
-
-    // ACTUAL VALUE
-    pwm_set_gpio_level(PROPELLER_PIN, 1800);
+    // pwm_set_gpio_level(PROPELLER_PIN, 1600);
+    // sleep_ms(600);
+    // pwm_set_gpio_level(PROPELLER_PIN, 1500); // THROTTLE AT ZERO
+    // sleep_ms(600);
+    // // ACTUAL VALUE
+    printf("Seting power to %d\n", power_level);
+    pwm_set_chan_level(prop->slice, prop->channel, power_level*WRAP_VAL/20000); 
+    // pwm_set_gpio_level(PROPELLER_PIN, 2000);
     // sleep_ms(20);
 }
+
--- robot-lib/src/propeller.h
+++ robot-lib/src/propeller.h
@@ -1,19 +1,31 @@
+#ifndef PROPELLER_H
+#define PROPELLER_H
+
 #include <stdint.h>
 #include <stdbool.h>
 
-#define PROPELLER_PIN 3
 
-#define CLOCK_DIVIDER 3 
-#define WRAP_VAL 1999 // for 50Hz FOR 24kHZ?????
+#define WRAP_VAL 20000 // for 50Hz FOR 24kHZ?????
 /**
- * Want a 50 Hz PWM Signal
+ * Want a 24 kHz PWM Signal
  * RP2040 default clock speed set at 125 MHz
  * Must divide clock speed to produce desired period 20 ms
  * PWM Freq 24 kHz = Clock Freq / (Clock Divider * (Wrap Val + 1))
 */
 #define MAX_PW 2000 // 2 ms.
+// 1500 is zero??
 #define MIN_PW 1000
+// #define PROPELLER_PIN_REAR 
 
-void propeller_init();
+typedef struct {
+    uint32_t slice;
+    uint32_t channel;
+    uint8_t pin;
+} propeller_t;
 
-void propeller_set_power(uint16_t power); // from 0 to 1.0?
+propeller_t* propeller_init(uint8_t pin);
+void propeller_arm_seq(propeller_t* prop);
+
+void propeller_set_power(propeller_t* prop, uint16_t power_level); // from 0 to 1.0?
+
+#endif // PROPELLER_H
