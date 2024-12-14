2ad42bb7d0b5eb7bb8d4e4b8031163be91aafb96
Henry Nguyen <henryhtvy@gmail.com>
2024-12-13 18:38:13 -0800

    Finish lf imu

--- robot-lib/src/mpu6050.h
+++ robot-lib/src/mpu6050.h
@@ -14,7 +14,7 @@ typedef struct {
     float az_offset;
 } mpu6050_t;
 
-mpu6050_t mpu6050_init(uint8_t sda_pin, uint8_t scl_pin);
+mpu6050_t* mpu6050_init(uint8_t sda_pin, uint8_t scl_pin);
 void mpu6050_update(mpu6050_t *mpu);
 void mpu6050_calibrate(mpu6050_t *mpu);
 
